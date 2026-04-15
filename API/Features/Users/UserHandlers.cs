4b62e5e03afc0d923049bd28a6b25cc0d6ea84a0
giovanisims <giovani@simoes.pro>
2026-04-14 22:25:31 -0300

    chore: refactor userhandler

--- API/Features/Users/UserHandlers.cs
+++ API/Features/Users/UserHandlers.cs
@@ -1,5 +1,4 @@
 using API.Infrastructure.Repositories;
-using API.Models;
 using MediatR;
 
 namespace API.Features.Users;
@@ -7,33 +6,26 @@ namespace API.Features.Users;
 public record RegisterUserCommand(string Name, string Email, string Password, string Cpf) : IRequest<User>;
 public record UpdateUserProfileCommand(int Id, string Name, string Email) : IRequest<bool>;
 
-public class UserHandlers :
+public class UserHandlers(IRepository<User> repository) :
     IRequestHandler<RegisterUserCommand, User>,
     IRequestHandler<UpdateUserProfileCommand, bool>
 {
-    private readonly IRepository<User> _repository;
-
-    public UserHandlers(IRepository<User> repository)
-    {
-        _repository = repository;
-    }
 
     public async Task<User> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
         var user = new User(request.Name, request.Email, request.Password, request.Cpf);
-        await _repository.AddAsync(user);
-        await _repository.SaveChangesAsync();
+        await repository.AddAsync(user);
+        await repository.SaveChangesAsync();
         return user;
     }
 
     public async Task<bool> Handle(UpdateUserProfileCommand request, CancellationToken cancellationToken)
     {
-        var user = await _repository.GetByIdAsync(request.Id);
-        if (user is null) return false;
+        if (await repository.GetByIdAsync(request.Id) is not { } user) return false;
 
         user.UpdateProfile(request.Name, request.Email);
-        await _repository.UpdateAsync(user);
-        await _repository.SaveChangesAsync();
+        await repository.UpdateAsync(user);
+        await repository.SaveChangesAsync();
         return true;
     }
 }
