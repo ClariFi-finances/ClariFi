53e4728611af24d3e2e57b2506756916dd14e32a
Lucas Brisch <lucasbrisch@gmail.com>
2026-05-27 19:24:25 -0300

    fix sidebar

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dc0ba2m7l9"
+    "revision": "0.bnfg5ma8dho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
